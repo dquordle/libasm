8d3313eb449b28a3f37c571982708be9c8a6ef35
Dewayne Quordlepleen <dquordle@et-b5.kzn.21-school.ru>
2021-02-28 16:12:34 +0300

    1

--- main.c
+++ main.c
@@ -12,25 +12,58 @@ char	*ft_strdup(char *s1);
 
 int main()
 {
-	char	*str = "Lol >= Kek";
 	int		i;
 	char	dst[100];
+	char	*str1;
+	char	*str2;
+	char	*str3;
+	ssize_t	len1;
+	ssize_t len2;
+	ssize_t len3;
 	
+	printf("let's get str1\n");
 	i = 0;
-	while (str[i])
+	len1 = ft_read(0, str1, 100);
+	str1[len1] = 0;
+	printf("ft_read return: %zd\n", len1);
+	while (str1[i])
 		i++;
 	printf("actual len : %d\n", i);
-	printf("ft_strlen : %d\n", ft_strlen(str));
-	printf("again : %d\n", ft_strlen(str));
-	printf("initial string : %s\n", str);
-	printf("strcpy return : %s\n", ft_strcpy(dst, str));
+	printf("ft_strlen : %d\n", ft_strlen(str1));
+	printf("again : %d\n", ft_strlen(str1));
+
+	printf("nice! let's go for a str2\n");
+	i = 0;
+	len2 = ft_read(0, str2, 100);
+	str2[len2] = 0;
+	printf("ft_read return: %zd\n", len2);
+	while (str2[i])
+		i++;
+	printf("actual len : %d\n", i);
+	printf("ft_strlen : %d\n", ft_strlen(str2));
+	printf("again : %d\n", ft_strlen(str2));
+
+	printf("great job, we got one more to go!\n");
+	i = 0;
+	len3 = ft_read(0, str3, 100);
+	str3[len3] = 0;
+	printf("ft_read return: %zd\n", len3);
+	while (str3[i])
+		i++;
+	printf("actual len : %d\n", i);
+	printf("ft_strlen : %d\n", ft_strlen(str3));
+	printf("again : %d\n", ft_strlen(str3));
+
+	printf("initial string : %s\n", str1);
+	printf("strcpy return : %s\n", ft_strcpy(dst, str1));
 	printf("strcpy dst : %s\n", dst);
-	printf("strcmp : %d\n", strcmp(str, dst));
-	dst[1] = 'a';
-	printf("my ft_strcmp : %d\n", ft_strcmp(str, dst));
+	printf("strcmp (same lines) : %d\n", strcmp(str1, dst));
+	printf("my ft_strcmp (same lines) : %d\n", ft_strcmp(str1, dst));
+	printf("strcmp (str2 & str3) : %d\n", strcmp(str2, str3));
+	printf("my ft_strcmp (str2 & str3) : %d\n", ft_strcmp(str2, str3));
+
 	printf("\n%zd\n", ft_write(1, "Keksik is the best", 17));
-	printf("%zd\n", ft_read(4, dst, 20));
-//	printf("%s\n", dst);
+	printf("ft_read return with wrong fd: %zd\n", ft_read(4, dst, 20));
 	char *mal = "Allocated";
 	char *all;
 	all = ft_strdup(mal);
